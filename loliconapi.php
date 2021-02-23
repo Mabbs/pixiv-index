7f6012e497e7c78abb6bb4d11462556fba2a12ee
Mayx <mayx@outlook.com>
2021-02-23 13:14:40 +0800

    Update loliconapi.php

--- loliconapi.php
+++ loliconapi.php
@@ -1,6 +1,6 @@
 <?php
 foreach(json_decode(file_get_contents("key.json"),true) as $key){
-$raw=json_decode(file_get_contents('https://api.lolicon.app/setu/?apikey='.$key.'&size1200=1&r18=1&num=10'),true)['data'];
+$raw='data';
 while($raw){
 $raw=json_decode(file_get_contents('https://api.lolicon.app/setu/?apikey='.$key.'&size1200=1&r18=1&num=10'),true)['data'];
 foreach($raw as $pic){
