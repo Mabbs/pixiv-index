bcbe15cdbb6525c8a331c6d21c76fe9ff6e7d3e7
Mayx <mayx@outlook.com>
2021-06-09 18:29:54 +0800

    Update loliconapi.php

--- loliconapi.php
+++ loliconapi.php
@@ -1,8 +1,6 @@
 <?php
-foreach(json_decode(file_get_contents("key.json"),true) as $key){
-$raw='data';
-while($raw){
-$raw=json_decode(file_get_contents('https://api.lolicon.app/setu/?apikey='.$key.'&size1200=1&r18=1&num=10'),true)['data'];
+for($i=0;$i<=50;$i++){
+$raw=json_decode(file_get_contents('https://api.lolicon.app/setu/?size1200=1&r18=1&num=100'),true)['data'];
 foreach($raw as $pic){
 $pic['url']=str_replace("https://i.pixiv.cat","",$pic['url']);
 if(!is_file('./data/'.$pic['pid'].'.json')){
@@ -10,7 +8,5 @@ $picfile = fopen('./data/'.$pic['pid'].'.json', "w");
 fwrite($picfile, json_encode($pic,JSON_UNESCAPED_UNICODE));
 fclose($picfile);
 }
-}
-//sleep(1);
 }
 }
