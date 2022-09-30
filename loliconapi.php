f4354b4fd5c8d0de616740cf8e7de59f7037ebad
Mayx <mayx@outlook.com>
2022-09-30 15:16:23 +0800

    Update loliconapi.php

--- loliconapi.php
+++ loliconapi.php
@@ -1,8 +1,9 @@
 <?php
 for($i=0;$i<=50;$i++){
-$raw=json_decode(file_get_contents('https://api.lolicon.app/setu/?size1200=1&r18=1&num=100'),true)['data'];
+$raw=json_decode(file_get_contents('https://api.lolicon.app/setu/v2?r18=1&num=20&size=regular'),true)['data'];
 foreach($raw as $pic){
-$pic['url']=str_replace("https://i.pixiv.cat","",$pic['url']);
+$pic['url']=$pic['urls']['regular']
+$pic['url']=str_replace("https://i.pixiv.re","",$pic['url']);
 if(!is_file('./data/'.$pic['pid'].'.json')){
 $picfile = fopen('./data/'.$pic['pid'].'.json', "w");
 fwrite($picfile, json_encode($pic,JSON_UNESCAPED_UNICODE));
